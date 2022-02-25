69463d3ba1c92380df61d030721151158f0743ca
Scott Gerlach <scott.gerlach@stackhawk.com>
2022-02-25 14:29:54 -0700

    Fix SQLi XSS for BigFix

--- src/main/java/hawk/service/SearchService.java
+++ src/main/java/hawk/service/SearchService.java
@@ -30,7 +30,7 @@ public class SearchService {
             public List<Item> execute(Connection connection) throws SQLException {
                 List<Item> items = new ArrayList<>();
                 // The wrong way
-                String query = "select id, name, description from ITEM where description like '%" +
+                /*String query = "select id, name, description from ITEM where description like '%" +
                         search.getSearchText() + "%'";
 
                 LOGGER.log(Level.INFO, "SQL Query " + query);
@@ -38,12 +38,14 @@ public class SearchService {
                         .createStatement()
                         .executeQuery(query);
 
+                 */
+
                 /* The righter way, should probably use built in Data Model for this, but this is safe */
-                /*String query = "select id, name, description from ITEM where description like ?";
+                String query = "select id, name, description from ITEM where description like ?";
                 PreparedStatement statement = connection.prepareStatement(query);
                 statement.setString(1, "%" + search.getSearchText() + "%");
                 LOGGER.log(Level.INFO, "SQL Query " + statement);
-                ResultSet rs = statement.executeQuery();*/
+                ResultSet rs = statement.executeQuery();
 
                 while (rs.next()) {
                     items.add(new Item(rs.getLong("id"), rs.getString("name"), rs.getString("description")));
